3fc30363785a9ddcdbcd412b5d6f333f59d31290
NinhMon2K <94120710+NinhMon2K@users.noreply.github.com>
2022-05-19 03:09:36 +0700

    3:09

---
+++ QLApp.Core.Entities/Dictionary/Data/cauhoi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLApp.Core.Entities.Dictionary.Data
+{
+   public class cauhoi
+    {
+        public int id { get; set; }
+        public string cauhoi { get; set; }
+        public string traloi { get; set; }
+    }
+}
---
+++ QLApp.Core.Entities/Dictionary/Data/people.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLApp.Core.Entities.Dictionary.Data
+{
+   public class people
+    {
+        public int id { get; set; }
+        public string tendaydu { get; set; }
+        public string anhdaidien { get; set; }
+        public DateTime ngaysinh { get; set; }
+        public string email { get; set; }
+        public int gioitinh { get; set; }
+        public string sdt { get; set; }
+    }
+}
