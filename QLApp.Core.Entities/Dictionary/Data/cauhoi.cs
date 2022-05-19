bfdf438c71b3ddaca74104e3254070c179f7437e
NinhMon2K <94120710+NinhMon2K@users.noreply.github.com>
2022-05-19 12:40:40 +0700

    12:40

--- QLApp.Core.Entities/Dictionary/Data/cauhoi.cs
+++ QLApp.Core.Entities/Dictionary/Data/cauhoi.cs
@@ -7,7 +7,7 @@ namespace QLApp.Core.Entities.Dictionary.Data
    public class cauhoi
     {
         public int id { get; set; }
-        public string cauhoi { get; set; }
+        public string cauhois { get; set; }
         public string traloi { get; set; }
     }
 }
--- QLApp.Core.Entities/Dictionary/User/User.cs
+++ QLApp.Core.Entities/Dictionary/User/User.cs
@@ -6,16 +6,11 @@ namespace QLApp.Core.Entities.Dictionary
 {
     public class User
     {
-        public String UserID { get; set; } = Guid.NewGuid().ToString();
-        public int UserCode { get; set; }
-        public String UserName { get; set; }
-        public String Password { get; set; }
-        public String Email { get; set; }
-        public String PhoneNumber { get; set; }
-        public String FullName { get; set; }
-        public String Image { get; set; }
-        public String CMT { get; set; }
-        public DateTime Birthday { get; set; }
+        public string tentaikhoan { get; set; } 
+        public string matkhau { get; set; }
+        public int idnguoidung { get; set; }
+        public int quyen { get; set; }
+       
       
     }
 }
